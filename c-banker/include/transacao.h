632bf96d72a7178d1b962b06256e056afafdec2a
Jeniffer Leme <162703146+jeniffer-leme@users.noreply.github.com>
2026-02-04 22:55:50 -0300

    Create transacao.h

---
+++ c-banker/include/transacao.h
@@ -0,0 +1,11 @@
+#define TIPO_DEPOSITO     1
+#define TIPO_SAQUE        2
+#define TIPO_TRANSFERENCIA 3
+
+typedef struct {
+    int conta_origem;
+    int conta_destino;      
+    int tipo;               
+    float valor;
+    char data[20];          
+} Transacao;
